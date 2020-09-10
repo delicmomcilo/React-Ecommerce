3e1132e2d544f0030545a0e744cba18d3bd3c3fd
Momcilo Delic <momcilodelic@gmail.com>
2020-09-11 00:48:34 +0200

    Pushing repo to git

---
+++ src/components/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Products from './Products'
+
+export default class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                <Products></Products>
+            </div>
+        )
+    }
+}
